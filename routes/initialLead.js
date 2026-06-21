53ee391fad0f46074b61806a170e0129ed95ccb5
Harsh Kumar Sinha <hksinha960@gmail.com>
2026-06-21 17:21:10 +0530

    check admin and a new route

--- routes/initialLead.js
+++ routes/initialLead.js
@@ -1,20 +1,21 @@
 import { Router } from "express";
 import {getAllInitalLeads,getInitialLeadById,createInitalLead,deleteInitialLead,updateInitialLead} from '../controller/initialLeadController.js';
 const router = Router();
-
+import checkAdmin from '../middleware/checkAdmin.js';
 // Get all the inital lead details
-router.get("/",getAllInitalLeads);
+router.get("/",checkAdmin,getAllInitalLeads);
 
 // Get inital Lead By ID
-router.get("/:id",getInitialLeadById);
+router.get("/:id",checkAdmin,getInitialLeadById);
 
 // Create New Inital Lead
-router.post("/",createInitalLead);
+router.post("/",checkAdmin,createInitalLead);
 
 // Delete Inital Lead by ID
-router.delete("/:id",deleteInitialLead);
+router.delete("/:id",checkAdmin,deleteInitialLead);
 
 // Update Inital Lead By ID
-router.patch("/:id",updateInitialLead);
+router.patch("/:id",checkAdmin,updateInitialLead);
 
+router.post("/req",createInitalLead);
 export default router;
